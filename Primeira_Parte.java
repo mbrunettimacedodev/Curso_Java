e301e7c658388910229b94171adeb01a277f89c1
Matheus Brunetti Macedo <mbrunettimacedo@gmail.com>
2025-02-26 11:57:07 -0300

    Primeira parte do curso

---
+++ Primeira_Parte.java
@@ -0,0 +1,90 @@
+public class Primeira_Parte {
+    // Java
+
+    /*oiiii */
+
+    /** 
+     * 1
+     * 2
+     * 3
+     * 4
+     * comentário para documentação */
+public static void main(String[] args){
+     String firstName = "Matheus";
+     String secondName = "Macedo";
+     String fullName = firstName + " " + secondName;
+     String full = "O nome é: " + fullName;
+
+     System.out.println(firstName + secondName);
+     System.out.println(fullName);
+     System.out.println(full);
+
+     char letra = 'A';
+     System.out.println(letra);
+
+     char simbolo = '$';
+     System.out.println(simbolo);
+
+     int numeroFixo = 560;
+     System.out.println(numeroFixo);
+
+    int n = 50;
+    System.out.println(n + 50);
+
+    System.out.println(n * 3);
+
+    System.out.println(n / 3);
+
+    long numeroGrandao = 500_000_000_000L;
+    long numeroGrande = 500000000000L;
+
+    System.out.println(numeroGrandao + 1);
+
+    double numeroComVirgula = 19.99D;
+
+    double preco = 77.890d;
+
+    double precoBaixo = 12.99;
+    System.out.println(numeroComVirgula);
+    System.out.println(preco);
+    System.out.println(precoBaixo);
+
+    double pi = 3.14_156_123;
+    System.out.println(pi);
+
+    // 9: Operadores Aritméticos
+
+    int soma = 12 + 5;
+
+    System.out.println(soma);
+    System.out.println(10 - 5);
+    System.out.println(10.0 / 3);
+    System.out.println(12.5 / 3);
+    System.out.println(10 * 3);
+    System.out.println(10 % 3);
+
+    // 10: Operadores Aritméticos 2
+
+    int x = 5;
+    x++;
+    x++;
+    x++;
+    x++;
+// loops For e While
+    System.out.println(x);
+
+    int y = 10;
+    y--;
+
+    System.out.println(y);
+
+    int a = 10;
+    a+= 5;
+    System.out.println(a);
+
+    int b = 15;
+    b-= 5;
+    System.out.println(b);
+    
+    }
+}
