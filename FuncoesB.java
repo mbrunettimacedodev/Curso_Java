4c41bfe8fe5f1f62b9642b8b7c8c1654c610b7c6
Matheus Macedo <mbrunettimacedodev@gmail.com>
2025-06-16 20:31:32 -0300

    Função

--- FuncoesB.java
+++ FuncoesB.java
@@ -6,6 +6,15 @@ public class FuncoesB {
         // 5 - funções com condicionais
         String r1 = verificarAcesso(19, true, false);
         System.out.println(r1);
+
+        // 6 - Funções com switch
+        System.out.println(obterDiaDaSemana(5));
+        System.out.println(obterDiaDaSemana(15));
+
+        // 7 - System exit
+        verificarAutenticacao("admin", "SenhaSegura");
+
+        System.out.println("Oi");
     }
 
     public static String verificarAcesso(int idade, boolean temCarteira, boolean temHistoricoNegativo){
@@ -16,5 +25,38 @@ public class FuncoesB {
         } else {
             return "Acesso negado: Critérios não atendidos 2";
         }
+    }
+
+    
+    public static String obterDiaDaSemana(int dia) {
+        switch(dia) {
+            case 1:
+            return "Segunda-Feira";
+            case 2:
+            return "Terça-Feira";
+            case 3:
+            return "Quarta-Feira";
+            case 4:
+            return "Quinta-Feira";
+            case 5:
+            return "Sexta-Feira";
+            case 6:
+            return "Sábado";
+            case 7:
+            return "Domingo";
+            
+            default:
+            return "Dia inválido";
+        }
+    }
+
+    public static void verificarAutenticacao(String usuario, String senha){
+
+        if(!usuario.equals("admin") && !senha.equals("SenhaSegura")){
+            System.out.println("Autenticação Falhou!");
+            System.exit(1);
+        }
+
+        System.out.println("Autenticação bem sucedida!");
     }
 }
