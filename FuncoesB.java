b39dfddbbbfd3b86925fc6f5c98607dcd16516ce
Matheus Macedo <matheus.macedo@artecola.com.br>
2025-06-17 13:31:12 -0300

    Escopo + Funções

--- FuncoesB.java
+++ FuncoesB.java
@@ -1,7 +1,10 @@
 public class FuncoesB {
-    
+     static int globalVar = 20;
+
     public static void main(String[] args) {
-        
+        // Escopo Global: 
+      
+        escopoLocal(globalVar);
 
         // 5 - funções com condicionais
         String r1 = verificarAcesso(19, true, false);
@@ -15,6 +18,29 @@ public class FuncoesB {
         verificarAutenticacao("admin", "SenhaSegura");
 
         System.out.println("Oi");
+
+        // 8 - Documentação Função
+        System.out.println(calcularMedia(6,5,8));
+
+        // 9 - Escopos
+
+        // Escopo local:
+        int localVar = 10;
+
+        if(true){
+            System.out.println(localVar);
+        }
+
+        System.out.println(globalVar);
+        
+        escopoLocal(localVar);
+
+        int testeFnEscopo = escopoLocal(localVar);
+
+        System.out.println(testeFnEscopo);
+
+        // { } geralmente delimitam um escopo, cria um escopo.
+
     }
 
     public static String verificarAcesso(int idade, boolean temCarteira, boolean temHistoricoNegativo){
@@ -52,11 +78,36 @@ public class FuncoesB {
 
     public static void verificarAutenticacao(String usuario, String senha){
 
-        if(!usuario.equals("admin") && !senha.equals("SenhaSegura")){
+        if(!usuario.equals("admin") || !senha.equals("SenhaSegura")){
             System.out.println("Autenticação Falhou!");
             System.exit(1);
         }
 
         System.out.println("Autenticação bem sucedida!");
     }
+
+/**
+ * 
+ * Calcula a média de três números inteiros:
+ * @param n1 O primeiro número/nota a ser enviado
+ * @param n2 O segundo número/nota a ser enviado
+ * @param n3 O terceiro número/nota a ser enviado
+ * @return A média dos três números
+ * 
+ */
+
+ public static double calcularMedia(int n1, int n2, int n3) {
+    return (n1 + n2 + n3) / 3;
+
+}
+
+public static int escopoLocal(int a){
+    System.out.println(globalVar);
+    System.out.println(a);
+
+    int testeFuncao = 1;
+
+    return testeFuncao;
+}
+
 }
